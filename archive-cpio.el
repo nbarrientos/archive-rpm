fd9bf7fef3113790e1b1cb5bdbc876786ef7bbad
Magnus Henoch <magnus.henoch@gmail.com>
2017-10-05 01:15:34 +0100

    Initial code checkin
    I've managed to open one RPM file with this.  More to be done...

---
+++ archive-cpio.el
@@ -0,0 +1,198 @@
+;;; archive-cpio.el --- CPIO support for archive-mode    -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2012  Stefan Monnier
+;; Copyright (C) 2017  Magnus Henoch
+
+;; Author: Magnus Henoch <magnus.henoch@gmail.com>
+;; Keywords: files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module adds support for CPIO archives to archive-mode.
+
+;; The format supported is the "New ASCII Format" as documented in
+;; http://people.freebsd.org/~kientzle/libarchive/man/cpio.5.txt.
+
+;; Based on cpio-mode.el by Stefan Monnier, posted here:
+;; https://lists.gnu.org/archive/html/emacs-devel/2015-04/msg00824.html
+
+;;; Code:
+
+(defconst archive-cpio-entry-header-re
+  "07070[12]\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)\\([[:xdigit:]]\\{8\\}\\)[[:xdigit:]]\\{8\\}\\|\0+\\'"
+  "Regular expression matching a CPIO entry.
+The matched groups are:
+
+1. ino
+2. mode
+3. uid
+4. gid
+5. nlink
+6. mtime
+7. filesize
+8. devmajor
+9. devminor
+10. rdevmajor
+11. rdevminor
+12. namesize
+
+The name starts at the end of the match, and goes on for namesize
+bytes.  It is padded with NUL bytes so that the start of file
+data is aligned to four bytes.  File data is also padded, so that
+the next header is aligned to four bytes.")
+
+;;;###autoload
+(defun archive-cpio-find-type ()
+  "Return `cpio' if the current buffer contains a CPIO archive.
+Otherwise, return nil.
+
+This function is meant to be used as a :before-until advice for
+`archive-find-type'."
+  (widen)
+  (goto-char (point-min))
+  (when (looking-at archive-cpio-entry-header-re)
+    'cpio))
+
+;;;###autoload
+(with-eval-after-load "arc-mode"
+  (advice-add 'archive-find-type :before-until 'archive-cpio-find-type))
+
+(defun archive-cpio--parse-mode (mode)
+  "Parse MODE, an integer, and return a permissions string (10 characters)."
+  (string
+   (case (logand #o170000 mode)
+     (#o140000 ?s)
+     (#o120000 ?l)
+     (#o100000 ?-)
+     (#o060000 ?b)
+     (#o040000 ?d)
+     (#o020000 ?c)
+     (#o010000 ?p)
+     (t (error "Unknown mode %S" mode)))
+   (if (zerop (logand #o400 mode)) ?- ?r)
+   (if (zerop (logand #o200 mode)) ?- ?w)
+   (if (zerop (logand #o100 mode))
+       (if (zerop (logand #o4000 mode)) ?- ?S)
+     (if (zerop (logand #o4000 mode)) ?x ?s))
+   (if (zerop (logand #o040 mode)) ?- ?r)
+   (if (zerop (logand #o020 mode)) ?- ?w)
+   (if (zerop (logand #o010 mode))
+       (if (zerop (logand #o2000 mode)) ?- ?S)
+     (if (zerop (logand #o2000 mode)) ?x ?s))
+   (if (zerop (logand #o004 mode)) ?- ?r)
+   (if (zerop (logand #o002 mode)) ?- ?w)
+   (if (zerop (logand #o001 mode)) ?- ?x)))
+
+(defun archive-cpio-summarize (&optional archive-buffer)
+  "Summarize files in a cpio archive.
+Insert file list in ARCHIVE-BUFFER, or the current buffer if
+ARCHIVE-BUFFER is nil."
+  (let (visual files)
+    (goto-char (point-min))
+    (while (not (eobp))
+      (assert (zerop (mod (- (point) (point-min)) 4)))
+      (cond
+       ((not (looking-at archive-cpio-entry-header-re))
+        (error "Unrecognized cpio header format"))
+       ((not (match-beginning 1))
+        ;; Reached the trailing padding, just skip it.
+        ;; (put-text-property (point) (point-max) 'invisible t)
+        (goto-char (match-end 0)))
+       (t
+        (let* ((ino (string-to-number (match-string 1) 16))
+               (mode (string-to-number (match-string 2) 16))
+               (uid (string-to-number (match-string 3) 16))
+               (gid (string-to-number (match-string 4) 16))
+               ;; (nlink (string-to-number (match-string 5) 16))
+               ;; (mtime (string-to-number (match-string 6) 16))
+               (filesize (string-to-number (match-string 7) 16))
+               ;; (devmajor (string-to-number (match-string 8) 16))
+               ;; (devminor (string-to-number (match-string 9) 16))
+               ;; (rdevmajor (string-to-number (match-string 10) 16))
+               ;; (rdevminor (string-to-number (match-string 11) 16))
+               (namesize (string-to-number (match-string 12) 16))
+               (namebeg (match-end 0))
+               (name (buffer-substring namebeg (+ namebeg namesize -1)))
+               (filebeg (+ (match-end 0) 2 (* (/ (+ namesize 1) 4) 4)))
+               (next (+ filebeg (* (/ (+ filesize 3) 4) 4))))
+          (if (and (zerop ino) (zerop mode) (zerop filesize)
+                   (equal name "TRAILER!!!"))
+              ;; Last entry in archive: go to end
+              (goto-char (point-max))
+            (let ((text
+                   (format "  %s %8.0f %10d/%-10d %s"
+                           (archive-cpio--parse-mode mode)
+                           filesize
+                           uid
+                           gid
+                           ;; (if (= (logand #o170000 mode) #o120000) ;Symlink
+                           ;;     ;; XXX
+                           ;;     " -> "
+                           ;;   "")
+                           name)))
+              (push (vector text (- (length text) namesize) (length text)) visual)
+              (push (vector name name nil mode filebeg) files)))
+          (goto-char next)))))
+    (with-current-buffer (or archive-buffer (current-buffer))
+      (goto-char (point-min))
+      (insert "M   Filemode   Length        UID/GID        File\n")
+      (insert "- ---------- -------- ---------- ---------- -----\n")
+      (archive-summarize-files (nreverse visual))
+      (apply #'vector (nreverse files)))))
+
+(defun archive-cpio-extract (archive name)
+  "Open the CPIO file ARCHIVE and extract NAME into the current buffer.
+This function is meant to be called from `archive-extract'."
+  ;; Based on archive-ar-extract
+  (let ((destbuf (current-buffer))
+        (archivebuf (find-file-noselect archive)))
+    (archive-cpio-extract-from-buffer name archivebuf destbuf)))
+
+(defun archive-cpio-extract-from-buffer (name archivebuf destbuf)
+  "Extract the file named NAME from the archive in ARCHIVEBUF.
+Insert the file contents into DESTBUF."
+  (with-current-buffer archivebuf
+    (let ((from nil) size)
+      (save-restriction
+        ;; We may be in archive-mode or not, so either with or without
+        ;; narrowing and with or without a prepended summary.
+        (save-excursion
+          (widen)
+          (search-forward-regexp archive-cpio-entry-header-re)
+          (goto-char (match-beginning 0))
+          (while (and (not from) (looking-at archive-cpio-entry-header-re))
+            (let* ((namesize (string-to-number (match-string 12) 16))
+                   (filesize (string-to-number (match-string 7) 16))
+                   (namebeg (match-end 0))
+                   (entry-name (buffer-substring namebeg (+ namebeg namesize -1)))
+                   (filebeg (+ (match-end 0) 2 (* (/ (+ namesize 1) 4) 4)))
+                   (next (+ filebeg (* (/ (+ filesize 3) 4) 4))))
+              (if (equal entry-name name)
+                  (setq from filebeg
+                        size filesize)
+                ;; Move to the end of the data.
+                (goto-char next)))))
+        (when from
+          (set-buffer-multibyte nil)
+          (with-current-buffer destbuf
+            ;; Do it within the `widen'.
+            (insert-buffer-substring archivebuf from (+ from size)))
+          (set-buffer-multibyte 'to)
+          ;; Inform the caller that the call succeeded.
+          t)))))
+
+(provide 'archive-cpio)
+;;; archive-cpio.el ends here
---
+++ archive-rpm.el
@@ -0,0 +1,217 @@
+;;; archive-rpm.el --- RPM support for archive-mode  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2017  Magnus Henoch
+
+;; Author: Magnus Henoch <magnus.henoch@gmail.com>
+;; Keywords: files
+
+;; This program is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; This module adds support for RPM archives to archive-mode.
+
+;; RPM files consist of metadata plus a compressed CPIO archive, so
+;; this module relies on `archive-cpio'.
+
+;;; Code:
+
+(require 'bindat)
+
+;;;###autoload
+(defun archive-rpm-find-type ()
+  "Return `rpm' if the current buffer contains an RPM archive.
+Otherwise, return nil.
+
+This function is meant to be used as a :before-until advice for
+`archive-find-type'."
+  (widen)
+  (goto-char (point-min))
+  ;; Look for RPM "lead" magic number, and version number 3.0
+  (when (looking-at "\xed\xab\xee\xdb\x03\x00")
+    'rpm))
+
+;;;###autoload
+(with-eval-after-load "arc-mode"
+  (advice-add 'archive-find-type :before-until 'archive-rpm-find-type))
+
+(defconst archive-rpm--header-bindat-spec
+  '((:magic              u24)
+    (:version            u8)
+    (:reserved           u32)
+    (:n-index-entries    u32)
+    (:data-len           u32))
+  "Bindat spec for RPM header.
+
+As per <http://ftp.rpm.org/max-rpm/s1-rpm-file-format-rpm-file-format.html>:
+
+The header structure header always starts with a three-byte magic
+number: 8e ad e8. Following this is a one-byte version
+number.  Next are four bytes that are reserved for future
+expansion.  After the reserved bytes, there is a four-byte number
+that indicates how many index entries exist in this header
+structure, followed by another four-byte number indicating how
+many bytes of data are part of the header structure.")
+
+(defconst archive-rpm--index-entry-bindat-spec
+  '((:tag        u32)
+    (:type       u32)
+    (:offset     u32)
+    (:count      u32))
+  "Bindat spec for index entry in RPM header.")
+
+(defun archive-rpm-summarize ()
+  "Summarize files in an RPM archive.
+This function is meant to be called by `archive-summarize'."
+  ;; Skip past "lead"
+  (goto-char (+ 96 (point-min)))
+  ;; Skip past the first header, the "signature", which is not
+  ;; particularly interesting.
+  (archive-rpm--parse-header)
+  ;; The second header contains data we're interested in.
+  (let* ((header-entries (archive-rpm--parse-header))
+         (payload-format (archive-rpm--get-header-data 1124 header-entries))
+         (payload-compressor (archive-rpm--get-header-data 1125 header-entries))
+         (payload (buffer-substring (point) (point-max)))
+         (archive-buffer (current-buffer)))
+    (unless (equal "cpio" payload-format)
+      (error "RPM payload is in `%s', not cpio format" payload-format))
+    (unless (equal "gzip" payload-compressor)
+      (error "RPM payload is compressed with `%s', not gzip" payload-compressor))
+    (unless (zlib-available-p)
+      (error "Zlib not available for RPM payload decompression"))
+    ;; Let's insert some interesting information, but in a way that it
+    ;; won't get clobbered...
+    (save-restriction
+      (goto-char (point-min))
+      (archive-rpm--insert-interesting-information header-entries)
+      (narrow-to-region (point) (point-max))
+      (with-temp-buffer
+        (set-buffer-multibyte nil)
+        (insert payload)
+        (unless (zlib-decompress-region (point-min) (point-max))
+          (error "Zlib decompression failed"))
+        ;; Finally ready to do something with the cpio archive
+        (archive-cpio-summarize archive-buffer)))))
+
+(defun archive-rpm-extract (archive name)
+  "Extract, from the RPM file ARCHIVE, the file named NAME."
+  ;; Based on archive-ar-extract
+  (let ((destbuf (current-buffer))
+        (archivebuf (find-file-noselect archive)))
+    (with-current-buffer archivebuf
+      (save-restriction
+        ;; We may be in archive-mode or not, so either with or without
+        ;; narrowing and with or without a prepended summary.
+        (save-excursion
+          (widen)
+          ;; Find and skip past "lead"
+          (search-forward "\xed\xab\xee\xdb\x03\x00")
+          (goto-char (+ 96 (match-beginning 0)))
+          (archive-rpm--parse-header)
+          (let* ((_header-entries (archive-rpm--parse-header))
+                 (payload (buffer-substring (point) (point-max))))
+            (with-temp-buffer
+              (set-buffer-multibyte nil)
+              (insert payload)
+              (unless (zlib-decompress-region (point-min) (point-max))
+                (error "Zlib decompression failed"))
+              (goto-char (point-min))
+              (archive-cpio-extract-from-buffer name (current-buffer) destbuf))))))))
+
+(defconst archive-rpm--interesting-fields
+  '((1000 . "Name")
+    (1001 . "Version")
+    (1002 . "Release")
+    (1004 . "Summary")
+    (1010 . "Distribution")
+    (1011 . "Vendor")
+    (1014 . "License")
+    (1016 . "Group")
+    (1020 . "URL")
+    (1021 . "OS")
+    (1022 . "Architecture"))
+  "Fields to output at top of RPM archive buffer.")
+
+(defun archive-rpm--insert-interesting-information (header-entries)
+  "Insert relevant and human-readable parts of HEADER-ENTRIES."
+  (let ((max-field-name-len (apply 'max
+                                   (mapcar (lambda (entry) (length (cdr entry)))
+                                           archive-rpm--interesting-fields))))
+    (dolist (field archive-rpm--interesting-fields)
+      (let ((data (archive-rpm--get-header-data (car field) header-entries)))
+        (when (stringp data)
+          (insert (cdr field)
+                  ":"
+                  (make-string (+ 1 (- max-field-name-len (length (cdr field)))) ?\s)
+                  data
+                  "\n"))))
+    (insert "\n")))
+
+(defun archive-rpm--get-header-data (tag header-entries)
+  "Return data from the header with tag TAG from HEADER-ENTRIES."
+  (bindat-get-field
+   (cl-find tag header-entries
+            :key (lambda (e) (bindat-get-field e :tag)))
+   :data))
+
+(defun archive-rpm--find-index-entry-data (index-entry data-starts-at data-len)
+  "Extract data for INDEX-ENTRY from RPM header data store.
+DATA-STARTS-AT is the position where the data store starts.
+DATA-LEN is the length of the data store."
+  (let ((entry-data-starts-at (+ data-starts-at (bindat-get-field index-entry :offset))))
+    (cl-case (bindat-get-field index-entry :type)
+      (6
+       ;; STRING (null-terminated)
+       (save-excursion
+         (goto-char entry-data-starts-at)
+         (when (search-forward "\0" (+ data-starts-at data-len) t)
+           (cons
+            (cons :data (buffer-substring entry-data-starts-at (match-beginning 0)))
+            index-entry))))
+      (otherwise
+       ;; Any other type.  Not parsing it, not adding :data.
+       index-entry))))
+
+(defun archive-rpm--parse-header ()
+  "Parse RPM header, and return list of index entries."
+  (unless (looking-at "\x8e\xad\xe8")
+    (error "Incorrect header magic"))
+  (let* ((header (bindat-unpack archive-rpm--header-bindat-spec
+                                (string-to-unibyte
+                                 (buffer-substring (point) (+ 16 (point))))))
+         (n-index-entries (bindat-get-field header :n-index-entries))
+         (data-len (bindat-get-field header :data-len))
+         (data-starts-at (+ (point) 16 (* 16 n-index-entries)))
+         index-entries)
+    ;; Move past header
+    (forward-char 16)
+    ;; Parse each index entry
+    (dotimes (_n n-index-entries)
+      (let ((index-entry (bindat-unpack archive-rpm--index-entry-bindat-spec
+                                        (string-to-unibyte
+                                         (buffer-substring (point) (+ 16 (point)))))))
+        (setq index-entry (archive-rpm--find-index-entry-data
+                           index-entry data-starts-at data-len))
+        (push index-entry index-entries)
+        (forward-char 16)))
+    ;; Skip past data store, and past any padding for 8-byte alignment
+    (forward-char (if (zerop (% data-len 8))
+                      data-len
+                    (+ data-len (- 8 (% data-len 8)))))
+    ;; Return list of index entries
+    index-entries))
+
+(provide 'archive-rpm)
+;;; archive-rpm.el ends here

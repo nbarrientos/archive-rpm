830158cfb3b43c85cfcb4bd5b92d4457d015c80a
Magnus Henoch <magnus.henoch@gmail.com>
2017-10-05 23:48:59 +0100

    Depend on Emacs 24.4 for advice-add

--- archive-rpm-pkg.el
+++ archive-rpm-pkg.el
@@ -1,1 +1,3 @@
-(define-package "archive-rpm" "0.1" "RPM and CPIO support for archive-mode" nil)
+(define-package "archive-rpm" "0.1" "RPM and CPIO support for archive-mode"
+  ;; Depend on Emacs 24.4 for `advice-add'
+  '((emacs "24.4")))
